2962ac8d6184955d121d6323c4f218e49af7ddbb
usman <usman.ali@crowdics.com>
2021-03-01 11:28:20 -0800

    openCV basic setup and images.videos

---
+++ read.py
@@ -0,0 +1,27 @@
+import cv2 as cv
+
+# read images
+# img = cv.imread('Photos/cat_large.jpg')
+
+# cv.imshow('cat', img)
+
+# keyboard binding function
+# cv.waitKey(0)
+
+# read videos
+
+capture = cv.VideoCapture('Videos/dog.mp4')
+
+while True:
+    is_true, frame = capture.read()
+    # show each frame
+    cv.imshow('Video', frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+
+# release and close all windows
+capture.release()
+cv.destroyAllWindows()
+
